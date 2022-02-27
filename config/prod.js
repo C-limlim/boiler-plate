b88480d38a906c347c3614fdbfaef92e1d3aa026
C-limlim <cofla2020@yonsei.ac.kr>
2022-02-27 17:40:06 +0900

    build:비밀설정 정보 관리 -  config/key.js prod.js 추가 , index.js 수정

---
+++ config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+	//after deploy
+	//헤로쿠 같은 사이트에서 가져옴
+	mongoURI : process.env.MONGO_URI
+}
--- index.js
+++ index.js
@@ -2,15 +2,35 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+const config = require('./config/key')
+
+//application/x-ww-form-urlencoded, json 분석해서 가져옴
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://cofla0317:12341234@boilerplate.zsx1n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
 	//modgodb ver 6.0 이상 지원 안한다고 하네요
 	//useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err));
 
 
-app.get('/', (req, res) => res.send('hello world'));
+app.get('/', (req, res) => res.send('hello world everyone'));
+
+app.post('/register', (req, res) => {
+	//get data for clients
+	//put data on database
+	const user = new User(req.body)
+	user.save((err, userInfo) => {
+		if (err) return res.json({success: false, err})
+		return res.status(200).json({
+			success: true
+		})
+	})
+})
 
 app.listen(port, () => {
 	console.log(`example app listing on port ${port}`);
